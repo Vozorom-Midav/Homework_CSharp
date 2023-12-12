b917d8144c75413a5e72d2f9a04bea3fd49a2601
Vad <bionicmas@yandex.ru>
2023-12-12 23:28:22 +0300

    Create string_homework

---
+++ string_and_char_homework/homework_0/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 1: Задайте двумерный массив символов (тип char[,]).
+//Создать строку из символов этого массива.
+
+string GetCharString(char[,] myChar)
+{
+    string str = string.Empty;
+
+    for(int i = 0; i<myChar.GetLength(0); i++)
+        for(int j = 0; j<myChar.GetLength(1); j++)
+        {
+            str += myChar[i,j];
+        }
+    return str;
+}
+
+
+char[,] myChar = {{'a', 'b', 'c'},{'d', 'e', 'f'}};
+
+string myText = GetCharString(myChar);
+
+Console.WriteLine(myText);
