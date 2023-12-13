025e735ee5e9e59ebca3b64f0d83988ee4e6a9a4
Vad <bionicmas@yandex.ru>
2023-12-13 19:52:29 +0300

    I finished my homework number 6

---
+++ string_and_char_homework/homework_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 3: Задайте произвольную строку. Выясните,
+//является ли она палиндромом.
+
+void ReversString (string myStr)
+{
+    string revStr = "";
+
+    for(int i = 1; i <= myStr.Length; i++)
+        revStr += myStr[myStr.Length - i];
+    
+    if(revStr == myStr)
+        Console.WriteLine("Строка является палиндромом");
+    else
+        Console.WriteLine("Строка не является палиндромом");
+}
+
+
+string str = "Шалаш";
+
+Console.WriteLine("Исходная строка: " + str);
+ReversString(str.ToLower());
